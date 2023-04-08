f86ea67ba8a0441e0d903030e03d2189f26f1b64
Dylan-LeBlanc <dylanleblanc213@gmail.com>
2023-04-08 10:58:11 -0500

    first commit

---
+++ build/CMakeFiles/sd_fat_spi.dir/lib/no-OS-FatFS-SD-SPI-RPi-Pico/FatFs_SPI/sd_driver/crc.c.obj.d
@@ -0,0 +1,4 @@
+CMakeFiles/sd_fat_spi.dir/lib/no-OS-FatFS-SD-SPI-RPi-Pico/FatFs_SPI/sd_driver/crc.c.obj: \
+ C:\Users\dylan\Documents\Rapberry_Pi_Work\sd_fat_spi\lib\no-OS-FatFS-SD-SPI-RPi-Pico\FatFs_SPI\sd_driver\crc.c \
+ C:\Users\dylan\Documents\Rapberry_Pi_Work\sd_fat_spi\lib\no-OS-FatFS-SD-SPI-RPi-Pico\FatFs_SPI\sd_driver\crc.h \
+ c:\vsarm\armcc\12977f~1.2mp\lib\gcc\arm-none-eabi\12.2.1\include\stddef.h
---
+++ build/CMakeFiles/sd_fat_spi.dir/lib/no-OS-FatFS-SD-SPI-RPi-Pico/FatFs_SPI/src/my_debug.c.obj.d
@@ -0,0 +1,20 @@
+CMakeFiles/sd_fat_spi.dir/lib/no-OS-FatFS-SD-SPI-RPi-Pico/FatFs_SPI/src/my_debug.c.obj: \
+ C:\Users\dylan\Documents\Rapberry_Pi_Work\sd_fat_spi\lib\no-OS-FatFS-SD-SPI-RPi-Pico\FatFs_SPI\src\my_debug.c \
+ c:\vsarm\armcc\12977f~1.2mp\arm-none-eabi\include\stdio.h \
+ c:\vsarm\armcc\12977f~1.2mp\arm-none-eabi\include\_ansi.h \
+ c:\vsarm\armcc\12977f~1.2mp\arm-none-eabi\include\newlib.h \
+ c:\vsarm\armcc\12977f~1.2mp\arm-none-eabi\include\_newlib_version.h \
+ c:\vsarm\armcc\12977f~1.2mp\arm-none-eabi\include\sys\config.h \
+ c:\vsarm\armcc\12977f~1.2mp\arm-none-eabi\include\machine\ieeefp.h \
+ c:\vsarm\armcc\12977f~1.2mp\arm-none-eabi\include\sys\features.h \
+ c:\vsarm\armcc\12977f~1.2mp\arm-none-eabi\include\sys\cdefs.h \
+ c:\vsarm\armcc\12977f~1.2mp\arm-none-eabi\include\machine\_default_types.h \
+ c:\vsarm\armcc\12977f~1.2mp\lib\gcc\arm-none-eabi\12.2.1\include\stddef.h \
+ c:\vsarm\armcc\12977f~1.2mp\lib\gcc\arm-none-eabi\12.2.1\include\stdarg.h \
+ c:\vsarm\armcc\12977f~1.2mp\arm-none-eabi\include\sys\reent.h \
+ c:\vsarm\armcc\12977f~1.2mp\arm-none-eabi\include\_ansi.h \
+ c:\vsarm\armcc\12977f~1.2mp\arm-none-eabi\include\sys\_types.h \
+ c:\vsarm\armcc\12977f~1.2mp\arm-none-eabi\include\machine\_types.h \
+ c:\vsarm\armcc\12977f~1.2mp\arm-none-eabi\include\sys\lock.h \
+ c:\vsarm\armcc\12977f~1.2mp\arm-none-eabi\include\sys\stdio.h \
+ C:\Users\dylan\Documents\Rapberry_Pi_Work\sd_fat_spi\lib\no-OS-FatFS-SD-SPI-RPi-Pico\FatFs_SPI\include/my_debug.h
